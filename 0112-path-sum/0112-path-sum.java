9bf40de6beac32028f58241981e1d5a7aace8759
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2023-04-14 11:25:01 +0530

    Time: 0 ms (100.00%), Space: 43 MB (12.64%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,27 +14,9 @@
  * }
  */
 class Solution {
-    public int value=0;
-    public boolean flag=false;
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root==null) return false;
-        Path(root,targetSum);
-        return flag;
-    }
-    public void Path(TreeNode root, int target) {
-        value+=root.val;
-        if(root.left==null && root.right==null){
-            if(value==target) flag=true;
-            return ;
-        }
-        
-        if(root.left!=null){
-        Path(root.left,target);
-        value-=root.left.val;    
-        }
-        if(root.right!=null){
-        Path(root.right,target);
-        value-=root.right.val;    
-        }
+        if(root.left==null && root.right==null) return targetSum-root.val==0;
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
     }
 }
