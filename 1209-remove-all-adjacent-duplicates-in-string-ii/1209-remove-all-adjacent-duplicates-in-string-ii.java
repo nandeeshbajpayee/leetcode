b93f1d34cd67f7aad5ffbfd81bf22c6d9859ba6c
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2023-05-30 14:41:09 +0530

    Time: 148 ms (15.74%), Space: 44.5 MB (38.09%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,57 @@
+import java.util.Stack;
+
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<Pair<Character, Integer>> stack = new Stack<>();
+
+        if (s.length() < k) {
+            return s;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (stack.isEmpty() || stack.peek().getFirst() != ch) {
+                stack.push(new Pair<>(ch, 1));
+            } else {
+                int count = stack.pop().getSecond() + 1;
+                if (count < k) {
+                    stack.push(new Pair<>(ch, count));
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            Pair<Character, Integer> pair = stack.pop();
+            char ch = pair.getFirst();
+            int count = pair.getSecond();
+            while (count > 0) {
+                sb.insert(0, ch);
+                count--;
+            }
+        }
+
+        return sb.toString();
+    }
+}
+
+class Pair<T, U> {
+    private T first;
+    private U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+}
+
+
