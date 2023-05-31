c3bbdaca014acac604ba0383291602c4bdf2439e
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2023-05-31 11:27:55 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (14.24%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,23 +1,20 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int count = 1;
-        int j = 1; // index to track the new length of the modified array
-
-        if (nums.length <= 2) return nums.length;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                count++; // increment the count for consecutive duplicates
-            } else {
-                count = 1; // reset the count for a new number
+        int j=1;
+        int count=1;
+        if(nums.length<2) return nums.length;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]==nums[i]){
+                count++;
             }
-
-            if (count <= 2) {
-                nums[j] = nums[i]; // overwrite the element at index j with the current number
-                j++; // increment the index j
+            else{
+                count=1;
+            }
+            if(count<=2){
+                nums[j]=nums[i];
+                j++;
             }
         }
-
-        return j; // return the new length of the modified array
+        return j;
     }
 }
