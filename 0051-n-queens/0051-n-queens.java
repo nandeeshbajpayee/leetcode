452f1734cc0812b580e5e978c141dfee704f57f2
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2023-06-10 07:26:42 +0530

    Time: 41 ms (5.06%), Space: 44.3 MB (7.95%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,64 @@
+
+class Solution {
+    List<List<String>> res=new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        helper(n);
+        return res;
+    }
+    List<String> list=new ArrayList<>();
+    public void helper(int n){
+        if(list.size()==n){
+            res.add(new ArrayList<>(list));
+            return ;
+        }
+        for(int i=0;i<n;i++){
+            String str=maker(i,n);
+            if(check(list,i)) list.add(str);
+            else continue;
+            helper(n);
+            list.remove(list.size()-1);
+        }
+    }
+    public String maker(int i,int n){
+        String str="";
+        for(int j=0;j<n;j++){
+            if(j==i) str+="Q";
+            else str+=".";
+        }
+        return str;
+    }
+      public boolean check(List<String> list, int J) {
+        int I = list.size();
+        int i = I;
+        int j = J;
+        i--;
+        j--;
+        while (i >= 0 && j >= 0) {
+            if (list.get(i).charAt(j) == 'Q')
+                return false;
+            i--;
+            j--;
+        }
+
+        i = I;
+        j = J;
+        i--;
+        while (i >= 0) {
+            if (list.get(i).charAt(j) == 'Q')
+                return false;
+            i--;
+        }
+
+        i = I;
+        j = J;
+        i--;
+        j++;
+        while (i >= 0 && j < list.get(i).length()) {
+            if (list.get(i).charAt(j) == 'Q')
+                return false;
+            i--;
+            j++;
+        }
+        return true;
+    }
+}
