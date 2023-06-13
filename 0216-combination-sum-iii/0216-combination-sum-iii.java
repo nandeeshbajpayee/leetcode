f7175437625dae4b42ecd0b57405b777b5c45c7d
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2023-06-13 09:23:37 +0530

    Time: 1 ms (35.29%), Space: 40.2 MB (64.40%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<List<Integer>> res=new ArrayList();
+    List<Integer> list=new ArrayList();
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        combination(k,n,1);
+        return res;
+    }
+    void combination(int k,int n,int index){
+        if(list.size()==k && n==0){
+            res.add(new ArrayList(list));
+        }
+        for(int i=index;i<10;i++){
+            if(i<=n) list.add(i);
+            else return ;
+            combination(k,n-i,i+1);
+            list.remove(list.size()-1);
+        }
+    }
+}
