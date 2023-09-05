7604ca134d029caef55b51337fafed279533e00f
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2023-09-05 22:47:56 +0530

    Time: 1 ms (85.14%), Space: 44.7 MB (47.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,18 +1,19 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> Si=new HashSet();
-        Set<Integer> Sj=new HashSet();
+        int[] ari = new int[matrix.length];
+        int[] arj = new int[matrix[0].length];
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[i].length;j++){
                 if(matrix[i][j]==0){
-                     Si.add(i);
-                     Sj.add(j);
+                     ari[i]=1;
+                     arj[j]=1;
+                    
                 }   
             }
         }
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[i].length;j++){
-                if(Si.contains(i) || Sj.contains(j)) matrix[i][j]=0; 
+                if(ari[i]==1 || arj[j]==1) matrix[i][j]=0; 
             }
         }
     }
