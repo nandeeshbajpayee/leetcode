5232add8881853ad0519cea53c2c62d5f76b69b6
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2023-11-06 08:58:58 +0530

    Time: 3 ms (63.55%), Space: 43.3 MB (95.31%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,18 +1,21 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        for(int i=1;i<n;i++){
-            grid[0][i]+=grid[0][i-1];
-        }
+        if(grid==null) return 0;
+        int m = grid.length;
+        int n =grid[0].length;
+        int[][] dp=new int[m][n];
+        dp[0][0]=grid[0][0];
         for(int i=1;i<m;i++){
-            grid[i][0]+=grid[i-1][0];
+            dp[i][0]=grid[i][0]+dp[i-1][0];
+        }
+        for(int j=1;j<n;j++){
+            dp[0][j]=grid[0][j]+dp[0][j-1];
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                grid[i][j]+=Math.min(grid[i-1][j],grid[i][j-1]);
+                dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
             }
         }
-        return grid[m-1][n-1];
+        return dp[m-1][n-1];
     }
 }
