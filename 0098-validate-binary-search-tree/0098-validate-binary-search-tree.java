df21a04ad350c9bc970dba8ccdc258a9e9627973
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2024-01-10 19:34:22 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (63.38%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,18 +15,15 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
-
-    public boolean isValidBST(TreeNode root, long min, long max) {
-        if (root == null) {
-            return true;
-        }
-
-        if (root.val <= min || root.val >= max) {
-            return false;
-        }
-
-        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    public boolean isValidBST(TreeNode root,long min,long max) {
+        if(root==null ) return true;
+        
+        else if(root.val<=min || root.val>=max) return false;
+        
+        else return isValidBST(root.left,min,root.val)&&
+            isValidBST(root.right,root.val,max);
+        
     }
 }
