81710095fe195f5b888e4d1b0dd41880b11b9582
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2024-01-11 20:25:58 +0530

    Time: 0 ms (100.00%), Space: 41 MB (45.22%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,30 +1,24 @@
 class Solution {
     public boolean checkValidString(String s) {
-        Stack<Integer> open=new Stack<>();
-        Stack<Integer> star=new Stack<>();
-        
-        
+        int openmin=0;
+        int openmax=0;
         for(int i=0;i<s.length();i++){
-            
-            if(s.charAt(i)=='(') open.push(i);
-            
-            else if(s.charAt(i)==')') {
-                if(!open.isEmpty()) {
-                    open.pop();
-                }
-                else if(!star.isEmpty()){
-                    star.pop();
-                }
-                else return false; 
+            if(s.charAt(i)=='('){
+                openmin++;
+                openmax++;
             }
-            else star.push(i);
+            else if(s.charAt(i)==')'){
+                openmin--;
+                openmax--;
+            }
+            else {
+                openmin--;
+                openmax++;
+            }
+            
+            if(openmax<0) return false;
+            if(openmin<0) openmin=0;
         }
-        while(!open.isEmpty()){
-            if(star.isEmpty()) return false;
-            if(open.peek()>star.peek()) return false;
-            open.pop();
-            star.pop();
-        }
-        return true;
+        return openmin==0;
     }
 }
