5576336d6babaa637a3fce150ee8c0066606acb5
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2024-01-12 17:35:37 +0530

    Time: 3 ms (52.66%), Space: 45.8 MB (12.33%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int maxpos=0;
-        for(int i=0; i<nums.length; i++){
+        for(int i=0; i<nums.length-1; i++){
             maxpos=Math.max(i+nums[i],maxpos);
-            if(maxpos==i && i!=nums.length-1) return false;
+            if(maxpos==i ) return false;
         }
         return maxpos>=nums.length-1;
     }
