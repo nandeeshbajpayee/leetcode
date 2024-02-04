a08da33adfdb83f8d103c0229b11a93e86876906
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2024-02-04 13:41:28 +0530

    Time: 145 ms (10.59%), Space: 45.5 MB (20.70%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -10,30 +10,30 @@ class Solution {
         String ans="";
         int i=0,j=0;
         
-         while (j < s.length()) {
-            if (map.containsKey(s.charAt(j))) {
-                map.put(s.charAt(j), map.get(s.charAt(j)) - 1);
+        while(j<s.length()){
+            if(map.containsKey(s.charAt(j))){
+                map.put(s.charAt(j),map.get(s.charAt(j))-1);
             }
-
-            while (check(map) && i <= j) {
-                if (j - i + 1 < anslen) {
-                    ans = s.substring(i, j + 1);
-                    anslen = j - i + 1;
+            
+              
+            while(check(map) && i<=j){
+                
+                if(j-i+1<anslen){
+                ans=s.substring(i,j+1);
+                anslen=j-i+1;
                 }
-
+                
                 if (map.containsKey(s.charAt(i))) {
                     map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
                 }
-
                 i++;
             }
-
+            
             j++;
         }
         return ans;
     }
-
-    private static Boolean check(Map<Character, Integer> map) {
+   private static Boolean check(Map<Character, Integer> map) {
         for (int value : map.values()) {
             if (value > 0) {
                 return false;
