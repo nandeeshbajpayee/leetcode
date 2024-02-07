6dbf6734ee4626ee78177d04908e8a7581557dc4
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2024-02-07 14:31:03 +0530

    Time: 9 ms (9.69%), Space: 62.9 MB (73.53%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -19,7 +19,6 @@ class Solution {
         int right=minDepth(root.right);
         int left=minDepth(root.left);
         if(right==0 || left==0) return left+right+1;
-        if(left<right) return left+1;
-        return right+1;
+        return Math.min(left,right)+1;
     }
 }
