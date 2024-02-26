248992ae675346d739ab0e99327bf34982b954b8
Nandeesh Bajpayee <106214644+nandeeshbajpayee@users.noreply.github.com>
2024-02-26 14:44:53 +0530

    Time: 2 ms (99.47%), Space: 57.1 MB (46.63%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,83 +1,18 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        if(sum(gas)<sum(cost)) return -1;
-        int start=0;
-        int current=0;
+        int pos=-1;
+        int total=0;
+        int curr=0;
         for(int i=0;i<gas.length;i++){
-            current+=(gas[i]-cost[i]);
-            if(current<0){
-                start=i+1;
-                current=0;
+            int diff=gas[i]-cost[i];
+            curr+=diff;
+            total+=diff;
+            if(curr<0){
+                curr=0;
+                pos=i;
             }
         }
-        return start;
+        if(total>=0) return pos+1;
+        return -1;
     }
-    public int sum(int[] arr){
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
-        }
-        return sum;
-    }
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int j=0;
-//         int i=0;
-//         while(i<gas.length){
-//         int margin=0;
-//         int index=-1;
-//         while(i<gas.length){
-//             int qura=gas[i]-cost[i];
-//             if(qura>margin){
-//                 margin = qura;
-//                 index=i;
-//                 break;
-//             } 
-//             i++;
-//         }
-//         i++;
-//         if(index==-1) return -1;
-//         int ptr=index;
-//         int ted=gas[ptr];
-//         boolean flag=false;
-//         while(ted>=cost[ptr]){
-//             ted=ted-cost[ptr];
-//             ptr=(ptr+1)%gas.length;
-//             ted=ted+gas[ptr];
-//             if(ptr==index) {
-//                 flag=true;
-//                 break;
-//             }
-//         }
-//         if(flag) return index;
-//             j++;
-//         }
-//     }
